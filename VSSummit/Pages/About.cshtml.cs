a4bb926d37442d597ca3cd8823bf43d92396117b
Orlando Gomes <orlando.gomes@lambda3.com.br>
2019-06-20 22:46:29 -0300

    primeiro upload

---
+++ VSSummit/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VSSummit.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Pesquisa de Perfil do VS Summit 2019";
+        }
+    }
+}
